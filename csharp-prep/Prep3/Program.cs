82290758c65fa4ce21bf11c3fcc944434d928e90
unknown <mikemaster98@gmail.com>
2025-01-16 19:32:31 -0700

    hello

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool guessedCorrect = false;
+        int guessAttempts = 0;
+
+
+
+        Console.WriteLine("Welcome to the magic number game! Please select a number.");
+        int magicNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Your Magic Number is " + magicNumber);
+        Random r = new Random();
+        int randomNumber = r.Next(0, magicNumber);
+
+
+        Console.WriteLine("Guess a number.");
+        int userNumber = int.Parse(Console.ReadLine());
+        do{
+            
+            if (userNumber == randomNumber){
+                Console.WriteLine("Congratulations! You guessed the magic number! What a fun game this was.");
+                guessAttempts ++;
+                guessedCorrect = true;
+            }
+            else if (userNumber > randomNumber){
+                Console.WriteLine("Sorry! The number is lower than "+userNumber);
+                guessAttempts ++;
+                Console.WriteLine("Guess a number");
+                userNumber = int.Parse(Console.ReadLine());
+            }
+            else if (userNumber < randomNumber){
+                Console.WriteLine("Sorry! The number is higher than "+userNumber);
+                guessAttempts ++;
+                Console.WriteLine("Guess a number");
+                userNumber = int.Parse(Console.ReadLine());
+            }
+
+
+        }while(guessedCorrect == false);
+        Console.WriteLine("You guessed " + guessAttempts + " times");
     }
 }
