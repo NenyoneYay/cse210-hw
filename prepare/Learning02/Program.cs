4c5087bae6ceb128725a769531a863b1613edb48
unknown <mikemaster98@gmail.com>
2025-01-23 20:44:59 -0700

    igjhaogaj

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,17 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2018;
+        job1._endYear = 2022;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Michael Loftus";
+        resume1._jobs.Add(job1);
+        resume1.Display();
     }
 }
